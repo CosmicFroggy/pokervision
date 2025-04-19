33246f201eb4f2c08ec302f021e9cb0459b4ab7e
CosmicFroggy <mark.southern98@gmail.com>
2025-04-19 21:01:25 +0100

    copy analysis code from main.ipynb to main.py

--- main.py
+++ main.py
@@ -1,7 +1,60 @@
 import tkinter as tk
 import cv2
 from PIL import Image, ImageTk
+import kagglehub
+from ultralytics import YOLO
+import os
+import yaml
+from sklearn.cluster import DBSCAN
 
+# TODO: put this label generation code in an object somewhere!!
+
+# download playing card dataset using kagglehub
+data_path = kagglehub.dataset_download("andy8744/playing-cards-object-detection-dataset")
+
+# get dataset labels
+with open(data_path + "/data.yaml", "r") as file:
+	data = yaml.safe_load(file)
+
+labels = data["names"]
+
+
+# class for storing card information, could make cards persistent between
+# frames later
+class CardPrediction:
+	def __init__(self, position, left, right, top, bottom, class_id, suit_rank, confidence):
+		self.position = position
+		self.left = left
+		self.right = right
+		self.top = top
+		self.bottom = bottom
+		self.class_id = class_id
+		self.suit_rank = suit_rank
+		self.confidence = confidence
+		self.hand = -1
+
+
+	def draw(self, frame):
+		cv2.rectangle(frame, 
+				 	  (self.left, self.top),
+					  (self.right, self.bottom), 
+					  (0, 255, 0), 
+					  1)
+		cv2.circle(frame,
+				   self.position,
+				   3,
+				   (0, 255, 0),
+				   -1)
+		cv2.putText(frame, 
+					f"{self.suit_rank}({self.hand}): {self.confidence:.2f}", 
+					(self.left, self.top - 30), 
+					cv2.FONT_HERSHEY_SIMPLEX, 
+					0.5, 
+					(0, 255, 0), 
+					1, 
+					cv2.LINE_AA)
+
+# main app class
 class App:
 	def __init__(self):
 		self.root = tk.Tk()
@@ -29,9 +82,14 @@ class App:
 			cv2.CAP_PROP_CONTRAST: tk.IntVar(value=100)
 		}
 
+		# create model using previously trained weights
+		self.model = YOLO("CardDetector.pt")
+
+
 	def update_cam_settings(self):
 		for setting, value in self.cam_settings.items():
 			self.cam.set(setting, value.get())
+
 
 	def open_settings_window(self):
 		# vv disable settings button
@@ -78,16 +136,23 @@ class App:
 				 variable=self.cam_settings[cv2.CAP_PROP_CONTRAST],
 				 command= lambda _ : self.update_cam_settings()).pack()
 
+
 	def close_settings_window(self, window):
 		self.menu_bar.entryconfig("Settings", state="normal")
 		window.destroy()
+
 
 	def update(self):
 		# read image from camera
 		_, frame = self.cam.read()
 
-		# convert image to tkinter usable format
+		# TODO: does the model expect rgb or bgr??? 
+		# Tkinter needs rgb either way
 		frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+		frame = self.analyse_frame(frame)
+
+		# convert image to tkinter usable format
 		frame = ImageTk.PhotoImage(image=Image.fromarray(frame))
 
 		# update the video panel
@@ -96,10 +161,56 @@ class App:
 
 		# call update again after 10ms
 		self.root.after(10, self.update)
+
+
+	def analyse_frame(self, frame):
+		# detect cards
+		cards = []
+		prediction = next(self.model.track(frame, stream=True, verbose=False, persist=True))
+		if prediction.boxes.id != None:
+			for i, box_id in enumerate(prediction.boxes.id):
+				# get attributes of identified card
+				x1, y1, x2, y2 = prediction.boxes.xyxy[i]
+				x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+				cx, cy, _, _ = prediction.boxes.xywh[i]
+				cx, cy = int(cx), int(cy)
+				class_id = int(prediction.boxes.cls[i])
+				conf = prediction.boxes.conf[i]
+
+				# create new prediction object and add to list
+				cards.append(CardPrediction((cx, cy), x1, x2, y1, y2, class_id, labels[class_id], conf))
+
+			# using DBSCAN clustering algo to group card hands
+			positions = []
+			for card in cards:
+				positions.append(card.position)
+			hand_labels = DBSCAN(eps=100, min_samples=2).fit(positions).labels_
+			for i, label in enumerate(hand_labels):
+				cards[i].hand = label
+
+			# draw the card annotations
+			for card in cards:
+				card.draw(frame)
+
+			# draw hand clustering
+			for i, card1 in enumerate(cards):
+				if i == len(cards) - 1:
+					break
+				for card2 in cards[i+1:]:
+					if card1.hand != -1 and card1.hand == card2.hand:
+						cv2.line(frame, 
+								card1.position, 
+								card2.position, 
+								(0, 255, 0), 
+								2)
+			
+		return frame
 		
+
 	def run(self):
 		self.update()
 		self.root.mainloop()
+
 
 	def destroy(self):
 		self.cam.release()
