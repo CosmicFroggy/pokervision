d717b5941e994fb430feb31578446f4034515e8a
CosmicFroggy <mark.southern98@gmail.com>
2025-04-21 20:36:55 +0100

    hard code labels rather than download

--- src/card_detection/analysis_funcs.py
+++ src/card_detection/analysis_funcs.py
@@ -7,19 +7,8 @@ from sklearn.cluster import DBSCAN
 from card_detection import CardPrediction
 
 
-# TODO: put this label generation code in an object somewhere!!
+labels = ['10c', '10d', '10h', '10s', '2c', '2d', '2h', '2s', '3c', '3d', '3h', '3s', '4c', '4d', '4h', '4s', '5c', '5d', '5h', '5s', '6c', '6d', '6h', '6s', '7c', '7d', '7h', '7s', '8c', '8d', '8h', '8s', '9c', '9d', '9h', '9s', 'Ac', 'Ad', 'Ah', 'As', 'Jc', 'Jd', 'Jh', 'Js', 'Kc', 'Kd', 'Kh', 'Ks', 'Qc', 'Qd', 'Qh', 'Qs']
 
-# TODO: probably should hard code the labels so we don't need to download
-# the dataset just for the labels
-
-# download playing card dataset using kagglehub
-data_path = kagglehub.dataset_download("andy8744/playing-cards-object-detection-dataset")
-
-# get dataset labels
-with open(data_path + "/data.yaml", "r") as file:
-	data = yaml.safe_load(file)
-
-labels = data["names"]
 
 
 # create model using previously trained weights
