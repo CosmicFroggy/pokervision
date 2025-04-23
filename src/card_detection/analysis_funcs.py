bccb9072405e776ff6f720f273632a7a06e3858d
CosmicFroggy <mark.southern98@gmail.com>
2025-04-23 12:49:04 +0100

    update identify_hands function
    It can now recognise royal straight flushes

--- src/card_detection/analysis_funcs.py
+++ src/card_detection/analysis_funcs.py
@@ -2,7 +2,7 @@ import cv2
 from ultralytics import YOLO
 from sklearn.cluster import DBSCAN
 import distinctipy
-from pokerkit import ShortDeckHoldemLookup
+from pokerkit import StandardLookup
 
 
 LABELS = ['10c', '10d', '10h', '10s', '2c', '2d', '2h', '2s', '3c', '3d', '3h', '3s', '4c', '4d', '4h', '4s', '5c', '5d', '5h', '5s', '6c', '6d', '6h', '6s', '7c', '7d', '7h', '7s', '8c', '8d', '8h', '8s', '9c', '9d', '9h', '9s', 'Ac', 'Ad', 'Ah', 'As', 'Jc', 'Jd', 'Jh', 'Js', 'Kc', 'Kd', 'Kh', 'Ks', 'Qc', 'Qd', 'Qh', 'Qs']
@@ -193,18 +193,22 @@ def identifyHands(hands):
 				hand[i] = "T" + label[2]
 		
 		# evaluate hand using pokerkit
+		hand = "".join(hand)
 		try:
-			evaluation = str(ShortDeckHoldemLookup().get_entry("".join(hand)).label)
+			evaluation = str(StandardLookup().get_entry(hand).label)
 		except ValueError:
 			evaluation = "Invalid hand!"
+
+		# check for royal straight flushes
+		royals = ["T", "Q", "J", "K", "A"]
+		if evaluation == "Straight flush" and all([(royal in hand) for royal in royals]):
+			evaluation = "Royal " + evaluation.lower()
 		
 		evaluations.append(evaluation)
 	
 	return evaluations
 
 		
-
-
 def annotate(image, cards, hand_labels):
 	"""
 	Annotate the frame with the given card boxes and hand groupings. The colours of the drawn frames represent the hands, outliers have grey boxes.
